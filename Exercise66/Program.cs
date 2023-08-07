1a500644f8aeb1397c9d2296ff0e07187d2cc3d9
marlen-alishev <marlen.alishev@gmail.com>
2023-08-07 13:57:37 +0600

    The second HW is done

---
+++ Exercise66/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNumbers (int M, int N)
+{
+    if (Math.Min(M,N)!=Math.Max(M,N))
+    {
+        return Math.Min(M,N) + SumNumbers(Math.Min(M,N)+1, Math.Max(M,N));
+    }
+    if (Math.Min(M,N)==Math.Max(M,N)) 
+    {
+        return Math.Min(M,N);
+    }
+    else return 0;
+}
+
+Console.Write("Enter M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("The sum is " + SumNumbers(numberM,numberN));
